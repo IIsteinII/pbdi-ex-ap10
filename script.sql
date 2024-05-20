b6609058e427ed1cfe5209f9a42416719ebd552f
Artur Stein <arturstein2004@gmail.com>
2024-05-20 18:52:38 -0300

    feat(ap10): exercicio 1.2

--- script.sql
+++ script.sql
@@ -13,3 +13,20 @@ BEGIN
     END LOOP;
 END $$;
 
+-- 1.2 Write a program that reads 6 numbers. These numbers will only be positive or negative (disregard null values). Print the total number of positive numbers.
+-- Gerar inteiros no intervalo de -50 a 50
+
+DO $$
+DECLARE
+    num INTEGER;
+    cont_positivo INTEGER := 0;
+BEGIN
+    FOR i IN 1..6 LOOP
+        num := (CASE WHEN RANDOM() < 0.5 THEN -1 ELSE 1 END) * (FLOOR(RANDOM() * 101) - 50);
+        IF num > 0 THEN
+            cont_positivo := cont_positivo + 1;
+        END IF;
+        RAISE NOTICE 'Número: %', num;
+    END LOOP;
+    RAISE NOTICE 'Total de números positivos: %', cont_positivo;
+END $$;
